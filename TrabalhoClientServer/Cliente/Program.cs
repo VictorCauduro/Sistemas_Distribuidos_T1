b60478fe4b24de4f6c968561600b99af46ece1c6
Victor <victor.cauduro@acad.pucrs.br>
2019-04-09 21:34:42 -0300

    commit inicial

---
+++ TrabalhoClientServer/Cliente/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+
+namespace Cliente
+{
+    class Program
+    {
+              
+        // Main Method 
+        static void Main(string[] args)
+        {
+            //Cria lista de arquivos
+            string path = Directory.GetCurrentDirectory();
+            var pathCortado = path.Split("bin");
+            string pathGambiarra = pathCortado[0] + "Files";
+            List<string> listaArquivos = new List<string>();
+            try
+            {
+                foreach (string file in Directory.GetFiles(pathGambiarra))
+                    listaArquivos.Add(Path.GetFileName(file));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Lista de arquivos inexistente ou vazia!");
+            }
+            string arquivos = "";
+            if(listaArquivos.Count > 0)
+            {
+                foreach (var item in listaArquivos)
+                {
+                    arquivos = arquivos + " " + item;
+                }
+            }
+           
+            ExecutaCliente(arquivos);
+        }
+
+        // ExecuteClient() Method 
+        static void ExecutaCliente(string arquivos)
+        {
+                        
+            try
+            {
+                IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddr = ipHost.AddressList[0];
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+                // Creation TCP/IP Socket using  
+                // Socket Class Costructor 
+                Socket sender = new Socket(ipAddr.AddressFamily,
+                           SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                                      
+                    sender.Connect(localEndPoint);
+                    Console.WriteLine("Socket connected to -> {0} ",
+                                  sender.RemoteEndPoint.ToString());
+ 
+                    
+                    byte[] messageSent = Encoding.ASCII.GetBytes(ipAddr + ";" + arquivos);
+                    int byteSent = sender.Send(messageSent);
+
+                    // Data buffer 
+                    byte[] messageReceived = new byte[1024];
+
+                    // We receive the messagge using  
+                    // the method Receive(). This  
+                    // method returns number of bytes 
+                    // received, that we'll use to  
+                    // convert them to string 
+                    int byteRecv = sender.Receive(messageReceived);
+                    Console.WriteLine("Message from Server -> {0}",
+                          Encoding.ASCII.GetString(messageReceived,
+                                                     0, byteRecv));
+
+                    // Close Socket using  
+                    // the method Close() 
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                    Console.WriteLine("bruh moment 6#");
+                    Console.ReadLine();
+                }
+
+                // Manage of Socket's Exceptions 
+                catch (ArgumentNullException ane)
+                {
+
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+
+                catch (SocketException se)
+                {
+
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+            }
+
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ TrabalhoClientServer/Server/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+        static IPAddress ipAddr = ipHost.AddressList[0];
+        static IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+        static Socket listener = new Socket(ipAddr.AddressFamily,
+                         SocketType.Stream, ProtocolType.Tcp);
+
+        private static Mutex mut = new Mutex();
+        private const int numIterations = 1;
+        private const int numThreads = 3;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                while (true)
+                {
+
+                    Console.WriteLine("Aguardando conexão...");
+
+                    // Suspend while waiting for 
+                    // incoming connection Using  
+                    // Accept() method the server  
+                    // will accept connection of client 
+                    Socket clientSocket = listener.Accept();
+
+                    // Data buffer 
+                    byte[] bytes = new Byte[1024];
+                    string data = null;
+                    //ThreadStart aux = new ThreadStart()
+                    //Thread newThread = new Thread(new ThreadStart(CadastrarCliente(data, bytes, clientSocket)));
+                    while (true)
+                    {
+                        mut.WaitOne();
+                        int numByte = clientSocket.Receive(bytes);
+
+                        data += Encoding.ASCII.GetString(bytes,
+                                               0, numByte);
+                        if (data.Length > -1)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            mut.ReleaseMutex();
+
+                        }                            
+                    }
+
+                    Console.WriteLine("Text received -> {0} ", data);
+                    byte[] message = Encoding.ASCII.GetBytes("Test Server");
+
+                    // Send a message to Client  
+                    // using Send() method 
+                    clientSocket.Send(message);
+
+                    // Close client Socket using the 
+                    // Close() method. After closing, 
+                    // we can use the closed Socket  
+                    // for a new Client Connection 
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                    mut.ReleaseMutex();
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+
+        public static void CadastrarCliente(string data, byte[] bytes, Socket clientSocket)
+        {
+
+            int numByte = clientSocket.Receive(bytes);
+            data += Encoding.ASCII.GetString(bytes,
+                                   0, numByte);
+
+
+        }
+
+
+
+
+    }
+}
